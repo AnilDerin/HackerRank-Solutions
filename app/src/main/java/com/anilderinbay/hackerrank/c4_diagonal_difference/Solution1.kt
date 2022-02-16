7c83b4d5050d0ba113a94f8fc47666626f386574
aderinbay <aderinbay54@gmail.com>
2022-02-16 11:39:27 +0300

    Solution for diagonalDifference

--- app/src/main/java/com/anilderinbay/hackerrank/c4_diagonal_difference/Solution1.kt
+++ app/src/main/java/com/anilderinbay/hackerrank/c4_diagonal_difference/Solution1.kt
@@ -4,13 +4,10 @@ import kotlin.math.abs
 
 
 fun main() {
-    var a: Array<Int> = arrayOf(1, 2, 3)
-    var b: Array<Int> = arrayOf(4, 5, 6)
-    var c: Array<Int> = arrayOf(9, 8, 9)
-
-    var m: Array<Array<Int>> = arrayOf(a,b,c)
-
-
+    val a: Array<Int> = arrayOf(1, 2, 3)
+    val b: Array<Int> = arrayOf(4, 5, 6)
+    val c: Array<Int> = arrayOf(9, 8, 9)
+    val m: Array<Array<Int>> = arrayOf(a, b, c)
 
     println(diagonalDifference(m))
 }
@@ -25,7 +22,7 @@ fun diagonalDifference(arr: Array<Array<Int>>): Int {
 
     for (i in 0 until n) {
         diagonal1 += arr[i][i]
-        diagonal2 += arr[i][n-i-1]
+        diagonal2 += arr[i][n - i - 1]
 
     }
     return abs(diagonal1 - diagonal2)
