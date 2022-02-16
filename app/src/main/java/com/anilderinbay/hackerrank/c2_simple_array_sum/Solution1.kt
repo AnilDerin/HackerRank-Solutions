8ad2b45e57f1a2ca83e8756b2af0ce8cf1baea53
aderinbay <aderinbay54@gmail.com>
2022-02-16 16:00:22 +0300

    Solution for plusMinus

--- app/src/main/java/com/anilderinbay/hackerrank/c2_simple_array_sum/Solution1.kt
+++ app/src/main/java/com/anilderinbay/hackerrank/c2_simple_array_sum/Solution1.kt
@@ -1,13 +1,14 @@
 package com.anilderinbay.hackerrank.c2_simple_array_sum
 
 fun main() {
-    simpleArraySum(arrayOf(2,3,4,5,6))
+    simpleArraySum(arrayOf(2, 3, 4, 5, 6))
 }
+
 fun simpleArraySum(ar: Array<Int>): Int {
     // Write your code here
     var sum = 0
 
-    for (e in ar ) {
+    for (e in ar) {
         sum += e
     }
     println(sum)
--- app/src/main/java/com/anilderinbay/hackerrank/c3_a_very_big_sum/Solution1.kt
+++ app/src/main/java/com/anilderinbay/hackerrank/c3_a_very_big_sum/Solution1.kt
@@ -1,15 +1,14 @@
 package com.anilderinbay.hackerrank.c3_a_very_big_sum
 
-import com.anilderinbay.hackerrank.c2_simple_array_sum.simpleArraySum
-
 fun main() {
-    aVeryBigSum(arrayOf(1000000001,1000000002,1000000003,1000000004,1000000005))
+    aVeryBigSum(arrayOf(1000000001, 1000000002, 1000000003, 1000000004, 1000000005))
 }
+
 fun aVeryBigSum(ar: Array<Long>): Long {
     // Write your code here
 
     var sum = 0.0
-    for(e in ar) {
+    for (e in ar) {
         sum += e
     }
 
